041d777b651723d55ac3d1ec300c01fe21651b1f
yusufAcik <12yusufacik@gmail.com>
2023-07-30 19:27:35 +0300

    parallel execution123456

--- src/test/java/com/krafttech/utilities/Driver.java
+++ src/test/java/com/krafttech/utilities/Driver.java
@@ -1,5 +1,4 @@
 package com.krafttech.utilities;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
@@ -12,60 +11,62 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 public class Driver {
-    private Driver() {
-    }
-    //InheritableThreadLocal  --> this is like a container, bag, pool. This is a java class
-    //in this pool we can have separate objects for each thread
-    //for each thread, in InheritableThreadLocal we can have separate object for that thread
-    //driver class will provide separate webdriver object per thread
-    private static InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>();
+
+    private Driver() {}
+
+    private static WebDriver driver;
+
     public static WebDriver get() {
-        //if this thread doesn't have driver - create it and add to pool
-        if (driverPool.get() == null) {
-            //if we pass the driver from terminal then use that one
-            //if we do not pass the driver from terminal then use the one properties file
-            String browser = System.getProperty("browser") != null ? System.getProperty("browser") : ConfigurationReader.get("browser");
+        if (driver == null) {
+            String browser = ConfigurationReader.get("browser");
             switch (browser) {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
-                    driverPool.set(new ChromeDriver());
+                    driver = new ChromeDriver();
                     break;
                 case "chrome-headless":
                     WebDriverManager.chromedriver().setup();
-                    driverPool.set(new ChromeDriver(new ChromeOptions().setHeadless(true)));
+                    driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
                     break;
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
-                    driverPool.set(new FirefoxDriver());
+                    driver = new FirefoxDriver();
                     break;
                 case "firefox-headless":
                     WebDriverManager.firefoxdriver().setup();
-                    driverPool.set(new FirefoxDriver(new FirefoxOptions().setHeadless(true)));
+                    driver = new FirefoxDriver(new FirefoxOptions().setHeadless(true));
                     break;
                 case "ie":
                     if (!System.getProperty("os.name").toLowerCase().contains("windows"))
                         throw new WebDriverException("Your OS doesn't support Internet Explorer");
                     WebDriverManager.iedriver().setup();
-                    driverPool.set(new InternetExplorerDriver());
+                    driver = new InternetExplorerDriver();
                     break;
+
                 case "edge":
                     if (!System.getProperty("os.name").toLowerCase().contains("windows"))
                         throw new WebDriverException("Your OS doesn't support Edge");
                     WebDriverManager.edgedriver().setup();
-                    driverPool.set(new EdgeDriver());
+                    driver = new EdgeDriver();
                     break;
+
                 case "safari":
                     if (!System.getProperty("os.name").toLowerCase().contains("mac"))
                         throw new WebDriverException("Your OS doesn't support Safari");
                     WebDriverManager.getInstance(SafariDriver.class).setup();
-                    driverPool.set(new SafariDriver());
+                    driver = new SafariDriver();
                     break;
             }
+
         }
-        return driverPool.get();
+        return driver;
     }
+
     public static void closeDriver() {
-        driverPool.get().quit();
-        driverPool.remove();
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
     }
+
 }
